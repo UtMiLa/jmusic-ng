68ba0b741efab032d8ff62e78e076fb44ab56932
John Erik Rønne Ekberg <UtMiLa@users.noreply.github.com>
2022-11-08 10:32:29 +0100

    #1: demo for lib

--- projects/jmusic-ng-test/src/app/demo/demo.component.ts
+++ projects/jmusic-ng-test/src/app/demo/demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NoteType, NoteDirection, StaffDef, ClefType, Time, SimpleSequence, TupletSequence, Rational, RetrogradeSequence, CompositeSequence } from '../../../../../../jmusic-model/src/model';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
@@ -11,5 +11,46 @@ export class DemoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  tuplets = {
+    staves: [{
+           initialClef: { clefType: ClefType.G, line: -2 },
+           initialMeter: { count: 4, value: 4 },
+           initialKey: { accidental: -1, count: 3 },
+           voices:[
+             {
+               noteDirection: NoteDirection.Up,
+               content: new TupletSequence(
+                new SimpleSequence( "c''8 c''8 c''8 c''8 c''8"
+                //new SimpleSequence( "c''4 c''4 c''4 c''4 c''4"
+               ),
+               { numerator: 4, denominator: 5 }
+             )},
+               {
+               noteDirection: NoteDirection.Down,
+               content:
+               new CompositeSequence(
+                new RetrogradeSequence(
+                  new TupletSequence( new SimpleSequence("c'8 d'8 e'8 f'8 g'8 a'8"), { numerator: 2, denominator: 3 })),
+                new SimpleSequence("c'8 d'8 e'8 f'8 g'8 a'8")
+              )
+
+               //content: new SimpleSequence( "c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8")
+           },
+           ]
+       } as StaffDef,
+       {
+         initialClef: { clefType: ClefType.F, line: 2 },
+         initialMeter: { count: 4, value: 4 },
+         initialKey: { accidental: -1, count: 3 },
+         voices:[
+           {
+             noteDirection: NoteDirection.Up,
+             content: new SimpleSequence( "c4 c4 c4 c4 c4 c4 c4 c4 c4 c4"
+           )}
+         ]
+     } as StaffDef
+     ]
+   };
 
 }
--- projects/jmusic-ng/src/lib/jmusic-ng.component.ts
+++ projects/jmusic-ng/src/lib/jmusic-ng.component.ts
@@ -1,20 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ScoreDef } from '../../../../../jmusic-model/src/model';
+import { ScoreViewModel, scoreModelToViewModel } from '../../../../../jmusic-model/src/logical-view';
+import { PhysicalModel, viewModelToPhysical, StandardMetrics, renderOnCanvas, Metrics } from '../../../../../jmusic-model/src/physical-view';
 
+console.log(Component, scoreModelToViewModel, viewModelToPhysical, StandardMetrics);
 @Component({
   selector: 'mus-jmusic-ng',
-  template: `
-    <p>
-      jmusic-ng works!
-    </p>
+  template: `<div style="font-family: Emmentaler;">
+<canvas id="scoreCanvas" #scoreCanvas width="1000px"
+  [height]="canvasHeight"></canvas>
+  </div>
   `,
   styles: [
   ]
 })
 export class JmusicNgComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  @ViewChild('scoreCanvas')
+  scoreCanvas: ElementRef<HTMLCanvasElement> | undefined;
+
+  private _scoreDef: ScoreDef | undefined;
+  @Input()
+  public get scoreDef(): ScoreDef | undefined {
+    return this._scoreDef;
+  }
+  public set scoreDef(value: ScoreDef | undefined) {
+    this._scoreDef = value;
+    this.render();
+    /*if (!this.scoreCanvas) {
+      return;
+    }
+    if (value && this.scoreCanvas) {
+      const logicalModel = scoreModelToViewModel(value);
+      const physicalModel = viewModelToPhysical(logicalModel, this.settings);
+      renderOnCanvas(physicalModel, this.scoreCanvas.nativeElement, {
+        offsetX: 10,
+        offsetY: 40,
+        scaleX: this.scale,
+        scaleY: this.scale
+      });
+    } else {
+      renderOnCanvas({elements: []}, this.scoreCanvas.nativeElement, {
+        offsetX: 10,
+        offsetY: 40,
+        scaleX: this.scale,
+        scaleY: this.scale
+      });
+    }*/
+  }
+
+  scale = 1.2;
+  staffCount = 2;
+
+  settings = {} as Metrics;
+  /*new StandardMetrics({
+      staffLineWidth: 6,
+  });*/
 
   ngOnInit(): void {
+    this.settings = new StandardMetrics({
+      staffLineWidth: 6,
+    });
+  }
+
+  ngAfterViewInit() {
+    //console.log(this.scoreCanvas);
+
+    setTimeout(() => { // otherwise does not load fint in Chrome
+      this.render();
+    }, 10);
+
+  }
+
+  render() {
+    if (!this.scoreCanvas) return;
+    if (this._scoreDef) {
+      const logicalModel = scoreModelToViewModel(this._scoreDef);
+      const physicalModel = viewModelToPhysical(logicalModel, this.settings);
+      renderOnCanvas(physicalModel, this.scoreCanvas.nativeElement, {
+        offsetX: 10,
+        offsetY: 40,
+        scaleX: this.scale,
+        scaleY: this.scale
+      });
+    } else {
+      renderOnCanvas({elements: []}, this.scoreCanvas.nativeElement, {
+        offsetX: 10,
+        offsetY: 40,
+        scaleX: this.scale,
+        scaleY: this.scale
+      });
+    }
+  }
+
+  get canvasHeight(): number {
+    return 100 * this.scale * this.staffCount;
   }
 
 }
