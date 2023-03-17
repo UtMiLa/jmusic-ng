a8fbd9383295b8fe0ce48ed86f16d38a367ca69a
John Erik Ekberg <JOEK@adm.ku.dk>
2023-03-17 11:37:35 +0100

    transpose

--- projects/jmusic-ng-test/src/demodata/tuplets.ts
+++ projects/jmusic-ng-test/src/demodata/tuplets.ts
@@ -9,12 +9,16 @@ export const tupletVars = {
 export const tuplets = {
   content: [
     [
-      [{ function: 'Tuplet', args: {variable: 'soprano'}, extraArgs: [{ numerator: 4, denominator: 5 }] }],
+      [
+        { function: 'Tuplet', args: {variable: 'soprano'}, extraArgs: [{ numerator: 4, denominator: 5 }] },
+        { function: 'Transpose', args: [{variable: 'scale'}], extraArgs: [{interval: 2, alteration: -1}] }
+      ],
       [
         { function: 'Reverse', args: [
           { function: 'Tuplet', args: {variable: 'scale'}, extraArgs: [{ numerator: 2, denominator: 3 }] }
         ] },
-        {variable: 'scale'}
+        {variable: 'scale'},
+        { function: 'Transpose', args: [{variable: 'scale'}], extraArgs: [{interval: 2, alteration: -1}] }
       ]
     ],
     [{variable: 'bass'}]
