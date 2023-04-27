faa36bda303a001e99e415c673b17b18c113b39d
John Erik Ekberg <JOEK@adm.ku.dk>
2023-04-27 14:56:44 +0200

    ly editor with some bugs

--- projects/jmusic-ng-test/src/app/ly/ly-editor/ly-editor.component.ts
+++ projects/jmusic-ng-test/src/app/ly/ly-editor/ly-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 
+//todo: still cannot set caret correctly when changing error status
+
+
 @Component({
   selector: 'app-ly-editor',
   templateUrl: './ly-editor.component.html',
@@ -19,8 +22,13 @@ export class LyEditorComponent {
   public set lyText(value: string | undefined) {
     if (value !== this.changedText) {
       this._lyText = value;
+      this.updateLyTextLines(value ?? '');
     }
   }
+
+
+  lyTextLines?: {before: string; after: string; marked: string}[];
+
 
   private _error: any;
   @Input()
@@ -37,24 +45,25 @@ export class LyEditorComponent {
       this.errorText = JSON.stringify(this._error);
     }
 
-    console.log(this._error?.location, this.editor);
+    //console.log(this._error?.location, this.editor);
     if (this._error && this._error.location && this.editor) {
 
-      setTimeout(() => {
-        var range = document.createRange();
-        var sel = window.getSelection();
 
-        range.setStart(this.editor.nativeElement.childNodes[0], this._error.location.start.offset);
-        range.setEnd(this.editor.nativeElement.childNodes[0], this._error.location.end.offset);
-        //range.collapse(true);
 
-        if (sel) {
-          sel.removeAllRanges()
-          sel.addRange(range)
-        }
-
-      }, 200);
+      this.updateLyTextLines(this.lyText ?? '');
     }
+  }
+
+  updateLyTextLines(text: string) {
+    const errorLine = this._error?.location?.start?.line ?? Infinity;
+    const errorFrom = this._error?.location?.start?.column ?? 0;
+    const errorTo = this._error?.location?.end?.column ?? 0;
+    this.lyTextLines = text.split('\n').map((line, idx) => (
+      errorLine === idx + 1
+      ? {before: line.substring(0, errorFrom - 1), marked: line.substring(errorFrom - 1, errorTo - 1), after: line.substring(errorTo - 1) }
+      : {before: line, marked: '', after: ''}
+    ));
+    console.log(this.lyTextLines);
   }
 
   errorText = '';
@@ -64,12 +73,21 @@ export class LyEditorComponent {
   @Output()
   lyTextChange = new EventEmitter<string>();
 
-  setDivCaret(element: HTMLElement) {
+  setDivCaret(element: Node, indexes: number[], position: number) {
     //var el = document.getElementById("editable")
+    while (indexes.length) {
+      const index = indexes.pop() as number;
+      element = element.childNodes.item(index);
+    }
     var range = document.createRange()
     var sel = window.getSelection()
 
-    range.setStart(element.childNodes[0], 5)
+    if (element.textContent)
+      position = Math.min(position, element.textContent.length);
+
+    //console.log('range.setStart(element, position)', element, position);
+    range.setStart(element, position);
+    range.setEnd(element, position);
     range.collapse(true)
 
     if (sel) {
@@ -81,12 +99,61 @@ export class LyEditorComponent {
   divText: string = '';
 
   editableChanged(event: Event) {
-    console.log(event);
-    this.changedText = (event.target as HTMLPreElement).textContent ?? '';
+    //console.log(event);
+
+    var sel = window.getSelection();
+    //console.log('sel', sel);
+    let caretLine = 0;
+    let caretPos = 0;
+    const elms = [];
+    const indexes: number[] = [];
+
+    if (sel && sel.rangeCount) {
+      const range = sel.getRangeAt(0);
+      if (range.startContainer === range.endContainer) {
+        let caretLine = 0;
+        caretPos = range.startOffset;
+        //console.log('which line?', range, sel, event);
+        //console.log('planning caret', caretLine, caretPos, range.startOffset, range.startContainer);
+
+        let elm: Node | null = range.startContainer;
+        while (elm && elm !== event.target) {
+          elms.push(elm);
+          var index = Array.prototype.indexOf.call(elm.parentElement?.childNodes, elm);
+          indexes.push(index)
+          elm = elm.parentElement;
+        }
+        //console.log('planning caret', elms, indexes);
+      }
+    }
+
+    const children = (event.target as HTMLElement).children;
+    if (children && children.length) {
+      let textLines = [] as string[];
+      for(let i = 0; i < children.length; i++) {
+        textLines.push(children.item(i)?.textContent ?? '');
+      }
+      this.changedText = textLines.join('\n');
+
+    } else {
+      this.changedText = '';
+    }
+    //console.log(children, this.changedText);
     this.lyTextChange.emit(this.changedText);
+
     setTimeout(() => {
+
       //this.setDivCaret(event.target as HTMLPreElement);
-    }, 100);
+      this.updateLyTextLines(this.changedText ?? '');
+
+      setTimeout(() => {
+        if (indexes.length) {
+          //console.log('setting caret', caretLine, caretPos, indexes);
+          this.setDivCaret(event.target as HTMLElement, indexes, caretPos);
+        }
+      }, 1);
+
+    }, 1);
 
   }
 
