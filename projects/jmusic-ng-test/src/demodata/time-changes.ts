c49d196608a7c83aac58c1c1c4c55d3ed64bedd7
John Erik Ekberg <JOEK@adm.ku.dk>
2024-06-12 12:05:36 +0200

    demo meters

--- projects/jmusic-ng-test/src/demodata/time-changes.ts
+++ projects/jmusic-ng-test/src/demodata/time-changes.ts
@@ -8,9 +8,12 @@ export const meterModel = {
          initialKey: { accidental: -1, count: 0 },
          voices:[
            {
-             noteDirection: NoteDirection.Up,
-             contentDef:"c'8 c'8 c'8 c'8 c'8 c'8 c'8 c'8 \\meter 6/8 c'8 c'8 c'8 c'8 c'8 c'8 \\meter 3/4 c'8 c'8 c'8 c'8 c'8 c'8"
-           },
+            noteDirection: NoteDirection.Up,
+            contentDef: 'c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 ' +
+              '\\meter 4/4 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 ' +
+              '\\compoundMeter #\'((2 2) (1 8)) c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 ' +
+              '\\meter 6/8 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8 ' +
+              '\\meter 3/4 c\'8 c\'8 c\'8 c\'8 c\'8 c\'8'           },
          ]
      } as StaffDef,
 
